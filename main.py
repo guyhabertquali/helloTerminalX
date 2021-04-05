89581a2bae96ddff9ee1936b045b4c11b80c12b1
guy.habert <guy.habert@qualitestgroup.com>
2021-04-05 10:25:06 +0300

    commit 2

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 def hello_terminalx():
     print("hello terminalX")
+    print("hi")
 
 def byebye_terminalx():
     print("byebye terminalX")
     print("guy")
-    print("bye")
+
 
 if __name__ == '__main__':
     hello_terminalx()
